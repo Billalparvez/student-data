f2c65f7eb79cea532396d170a596488eccb7f498
billalparvez <billalparvez80@gmail.com>
2024-01-17 22:40:46 +0600

    completed design

--- src/Components/Student.jsx
+++ src/Components/Student.jsx
@@ -1,6 +1,5 @@
 import ClassSixTable from "./ClassSixTable";
 import TableHead from "./TableHead";
-import TableRow from "./tableRow";
 
 const Student = ({ six, seven, eight }) => {
   return (
@@ -10,15 +9,30 @@ const Student = ({ six, seven, eight }) => {
         <table className="table">
           {/* table head */}
           <TableHead></TableHead>
-
-          <TableRow headings={"Class Six"}></TableRow>
+          <tr className="bg-white/5">
+            <td
+              className="p-3 text-white text-sm md:text-xl bg-[#186149]"
+              colSpan="4"
+            >
+              Class Six
+            </td>
+          </tr>
+          {/* <TableRow headings={"Class Six"}></TableRow> */}
           {/* cls six data */}
           {six.map((data) => (
             <ClassSixTable key={data.id} data={data}></ClassSixTable>
           ))}
 
           {/* row */}
-          <TableRow headings={"Class Seven"}></TableRow>
+          {/* <TableRow headings={"Class Seven"}></TableRow> */}
+          <tr className="bg-white/5">
+            <td
+              className="p-3 text-white text-sm md:text-xl bg-[#186149]"
+              colSpan="4"
+            >
+              Class Seven
+            </td>
+          </tr>
 
           {/* cls seven data */}
           {seven.map((data) => (
@@ -26,8 +40,16 @@ const Student = ({ six, seven, eight }) => {
           ))}
 
           {/* row */}
-          <TableRow headings={"Class Eight"}></TableRow>
+          {/* <TableRow headings={"Class Eight"}></TableRow> */}
           {/* cls eight data */}
+          <tr className="bg-white/5">
+            <td
+              className="p-3 text-white text-sm md:text-xl bg-[#186149]"
+              colSpan="4"
+            >
+              Class Eight
+            </td>
+          </tr>
 
           {eight.map((data) => (
             <ClassSixTable key={data.id} data={data}></ClassSixTable>
